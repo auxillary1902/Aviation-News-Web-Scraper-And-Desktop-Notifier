235f562d00f742ca7deee8800a85e4a2e03615d8
Sudarshan Parthasarathy <omegacore123@yahoo.in>
2020-03-24 17:15:41 +0530

    add main function in notifier.py to call the notification functiion

--- notifier.py
+++ notifier.py
@@ -2,26 +2,29 @@ import time
 import notify2
 import json
 
-with open('newsData.json') as json_data:
-	jsonData = json.load(json_data)
+def displayNotification():
+	with open('newsData.json') as json_data:
+		jsonData = json.load(json_data)
 
 
-# for i in jsonData:
-# 	print(i['url'])
+	# for i in jsonData:
+	# 	print(i['url'])
 
-notify2.init('News Notifier')
-n = notify2.Notification(None)
+	notify2.init('News Notifier')
+	n = notify2.Notification(None)
 
-n.set_urgency(notify2.URGENCY_NORMAL)
+	n.set_urgency(notify2.URGENCY_NORMAL)
 
-n.set_timeout(10000)
+	n.set_timeout(10000)
 
-for newsItem in jsonData:
-	n.update('Aviation News Notification',newsItem['heading'])
-	n.show()
-	time.sleep(2)
+	for newsItem in jsonData:
+		n.update('Aviation News Notification',newsItem['heading'])
+		n.show()
+		time.sleep(2)
 
 
+if __name__ == '__main__':
+    displayNotification()
 
 # n.update('Aviation News Notification',"This is the description for the test notification")
 # n.show()
