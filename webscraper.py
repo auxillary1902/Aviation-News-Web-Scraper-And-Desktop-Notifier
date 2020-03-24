a0f7d187377caa708a595c0d1815de724acbf495
Sudarshan Parthasarathy <omegacore123@yahoo.in>
2020-03-24 17:12:15 +0530

    add main function in webscraper.py

--- webscraper.py
+++ webscraper.py
@@ -1,20 +1,23 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-url = 'https://simpleflying.com/category/aviation-news/'
-response = requests.get(url,timeout=5)
-content = BeautifulSoup(response.content,"html.parser")
-news = []
-for heading in content.findAll('h2',attrs={"class" : "entry-title"}) :
-	headingObject = {
-	 "url" : heading.find('a').encode('utf-8'),
-	 "heading" : heading.text.encode('utf-8')
-	}
-	print(headingObject)
-	news.append(headingObject)
 
-with open('newsData.json','w') as outfile:
-	json.dump(news,outfile)
+def scrape():
+  url = 'https://simpleflying.com/category/aviation-news/'
+  response = requests.get(url,timeout=5)
+  content = BeautifulSoup(response.content,"html.parser")
+  news = []
+  for heading in content.findAll('h2',attrs={"class" : "entry-title"}):
+     headingObject = {"url" : heading.find('a').encode('utf-8'),"heading" : heading.text.encode('utf-8')}
+	   
+	 # print(headingObject)
+     news.append(headingObject)
+
+  with open('newsData.json','w') as outfile:
+       json.dump(news,outfile)
+
+if __name__ == '__main__':
+   scrape()       
 
 # print(content)
 # print(headings)
